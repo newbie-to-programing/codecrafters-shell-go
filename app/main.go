9c9e1d2a5d1bda8e0bfd59d45fc203ce0875dedb
Tan Juin Hong <tanjuinhong@192.168.1.4>
2025-12-20 22:03:15 +0800

    Handle invalid commands

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,5 +11,9 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
+	//fmt.Print("$ ")
+
+	// Captures the user's command in the "command" variable
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
