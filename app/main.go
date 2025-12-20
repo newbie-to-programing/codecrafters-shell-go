74d76faf3eab9d59d13cc7aba05a224264c5afe8
Tan Juin Hong <tanjuinhong@192.168.1.4>
2025-12-20 22:09:59 +0800

    Implement a REPL

--- app/main.go
+++ app/main.go
@@ -10,10 +10,12 @@ import (
 var _ = fmt.Print
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
+	for true {
+		// TODO: Uncomment the code below to pass the first stage
+		fmt.Print("$ ")
 
-	// Captures the user's command in the "command" variable
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Println(command[:len(command)-1] + ": command not found")
+		// Captures the user's command in the "command" variable
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Println(command[:len(command)-1] + ": command not found")
+	}
 }
