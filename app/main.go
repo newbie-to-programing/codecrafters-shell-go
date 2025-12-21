132f22b08a22ca555e06e274c7af416a1de3d7d9
Tan Juin Hong <tanjuinhong@192.168.1.4>
2025-12-21 08:36:01 +0800

    Locate executable files

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"path/filepath"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -26,7 +27,43 @@ func main() {
 			if builtin == "echo" || builtin == "type" || builtin == "exit" {
 				fmt.Printf("%v is a shell builtin\n", builtin)
 			} else {
-				fmt.Printf("%v: not found\n", builtin)
+				pathList := "" // todo: get from input
+
+				dirs = filepath.SplitList(pathList)
+
+				hasFound := false
+				for _, dir := range dirs {
+					entries, err := os.ReadDir(dir)
+					if err != nil {
+						log.Printf("Could not read %s: %v", dir, err)
+						continue
+					}
+
+					for _, entry := range entries {
+						if entry.IsDir() {
+							continue
+						}
+
+						info, err := entry.Info()
+						if err != nil {
+							continue
+						}
+
+						mode := info.Mode()
+						if mode.Perm()&0111 != 0 {
+							fmt.Printf("%s is %s", builtin, filepath.Join(dir, entry.Name()))
+							hasFound = true
+						}
+					}
+
+					if hasFound {
+						break
+					}
+				}
+
+				if !hasFound {
+					fmt.Printf("%s: not found", builtin)
+				}
 			}
 		} else {
 			fmt.Println(command[:len(command)-1] + ": command not found")
