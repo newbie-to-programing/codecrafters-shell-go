bcf1d0202d736e04dd5d8a0e44d085b062722ae8
Tan Juin Hong <tanjuinhong@192.168.1.4>
2025-12-23 23:39:26 +0800

    fix bug

--- app/main.go
+++ app/main.go
@@ -25,44 +25,75 @@ func main() {
 			continue
 		}
 
-		var replaced string
-		var re *regexp.Regexp
-		var parts []string
-		cleaned := make([]string, 0, len(parts))
-		if strings.Contains(input, "\"") {
-			replaced = strings.ReplaceAll(input, "\"\"", "") // double quotes
-			re = regexp.MustCompile(`"[^"]*"|[^\s]+`)
-			parts = re.FindAllString(replaced, -1)
-			for _, p := range parts {
-				clean := strings.ReplaceAll(p, "\"", "")
-				cleaned = append(cleaned, clean)
-			}
-		} else {
-			replaced = strings.ReplaceAll(input, "''", "") // single quotes
-			re = regexp.MustCompile(`'[^']*'|[^\s]+`)
-			parts = re.FindAllString(replaced, -1)
-			for _, p := range parts {
-				clean := strings.ReplaceAll(p, "'", "")
-				cleaned = append(cleaned, clean)
-			}
+		// The pattern identifies 4 types of "blobs" that form arguments:
+		// 1. Double quotes: "(content)"
+		// 2. Single quotes: '(content)'
+		// 3. Escaped char: \. (outside quotes)
+		// 4. Normal text:   [^\s"'\\]+
+		pattern := `(?s)"([^"\\]*(?:\\.[^"\\]*)*)"|'([^']*)'|\\(.)|([^\s"'\\]+)`
+		re := regexp.MustCompile(pattern)
+
+		// Get indexes to handle concatenation (touching vs. separated by space)
+		allMatches := re.FindAllStringSubmatchIndex(input, -1)
+		if len(allMatches) == 0 {
+			return
 		}
 
-		command := cleaned[0]
-		args := cleaned[1:]
+		var args []string
+		var currentArg strings.Builder
+		var lastEnd int = -1
+
+		for _, m := range allMatches {
+			matchStart := m[0]
+			matchEnd := m[1]
+
+			// If there is a gap (whitespace) between this match and the last,
+			// the previous argument is finished.
+			if lastEnd != -1 && matchStart > lastEnd {
+				args = append(args, currentArg.String())
+				currentArg.Reset()
+			}
+
+			// Check which capturing group matched and extract the literal content
+			if m[2] != -1 {
+				// Group 1: Double Quotes. Content is input[m[2]:m[3]]
+				// Note: For now, we treat backslashes inside double quotes as literal
+				// per your current stage requirements.
+				currentArg.WriteString(input[m[2]:m[3]])
+			} else if m[4] != -1 {
+				// Group 2: Single Quotes. Content is input[m[4]:m[5]]
+				currentArg.WriteString(input[m[4]:m[5]])
+			} else if m[6] != -1 {
+				// Group 3: Backslash outside quotes. Content is input[m[6]:m[7]]
+				// This handles: \ , \n, \\, \' etc.
+				currentArg.WriteString(input[m[6]:m[7]])
+			} else if m[8] != -1 {
+				// Group 4: Normal unquoted text
+				currentArg.WriteString(input[m[8]:m[9]])
+			}
+
+			lastEnd = matchEnd
+		}
+
+		// Always add the final building argument
+		args = append(args, currentArg.String())
+
+		command := args[0]
+		otherArgs := args[1:]
 
 		switch command {
 		case ExitCommand:
 			os.Exit(0)
 		case EchoCommand:
-			fmt.Println(strings.Join(args, " "))
+			fmt.Println(strings.Join(otherArgs, " "))
 		case TypeCommand:
-			handleTypeCommand(args)
+			handleTypeCommand(otherArgs)
 		case PwdCommand:
 			handlePwdCommand()
 		case CdCommand:
-			handleCdCommand(args)
+			handleCdCommand(otherArgs)
 		default:
-			handleExternalCommand(command, args)
+			handleExternalCommand(command, otherArgs)
 		}
 	}
 }
